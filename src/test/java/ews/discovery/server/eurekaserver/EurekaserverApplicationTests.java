f4df23ec40b898fc540b63c7fff9c65fbbe73678
unknown <hereisjaishah@gmail.com>
2022-03-17 14:49:04 +0530

    Ews-Loader

---
+++ src/test/java/ews/discovery/server/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package ews.discovery.server.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
